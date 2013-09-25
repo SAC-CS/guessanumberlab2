1486cbff9bd546485641a39c88cb9e008a52db2c
juanphdz <juanphdez@gmail.com>
2013-09-24 20:52:10 -0700

    commit

---
+++ src/GuessANumberLab2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class GuessANumberLab2 {
+
+	public static void main(String[] args)
+	{
+	
+		int x = 0 + (int) ( Math.random() * 10 );
+		Scanner input = new Scanner( System.in );
+
+		//variables
+
+		System.out.println("Guess a number from 0-9: ");
+
+		int y = input.nextInt();
+
+		if (x == y)
+		{System.out.println("You are right!");}
+		else {
+			if (x < y )
+			{System.out.println("Too high guess a lower number");}
+			else {
+				if (x > y)
+				{System.out.println("Too low guess another number");}
+
+		}
+		}
+	}
+
+}
